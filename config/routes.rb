80d4fecbaad8e201de0135f8d42547ae0edda9ae
satuma <satuma.mixed@gmail.com>
2015-09-21 23:09:53 +0900

    included "BootStrap3"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+	root to: 'welcome#index'
+end
+  # get 'welcome/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +57,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+# end
